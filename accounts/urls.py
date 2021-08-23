344d1972ccf0b3612f661f6a4ec0d2ff76a567ae
EmonZaman <aagontukemon@gmail.com>
2021-08-23 23:34:25 +0600

    login error solved

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,13 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path, reverse_lazy
 
-from .views import MyLoginView, RegistrationView, AboutView
+from .views import LoginView, RegistrationView, AboutView
 
 app_name = "accounts"
 
 urlpatterns = [
-    path("login/", MyLoginView.as_view(), name="login"),
+    path("login/", LoginView.as_view(), name="login"),
     path("register/", RegistrationView.as_view(), name="register"),
-    path("logout/", LogoutView.as_view(next_page=reverse_lazy("accounts:login")),name="logout"),
+    path("logout/", LogoutView.as_view(next_page=reverse_lazy("accounts:login")), name="logout"),
     path('index/', AboutView.as_view(), name="index"),
- ]
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,19 @@
 from django.contrib.auth import logout
-from django.contrib.auth.views import LoginView
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView as AuthLoginView
 from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import CreateView, RedirectView
+from django.views.generic import CreateView
+from django.views.generic import TemplateView
 
 from accounts.forms import UserForm
 from accounts.models import User
 
 
-# Create your views here.
-from django.views.generic import TemplateView
-
 class AboutView(TemplateView):
     template_name = "accounts/index.html"
 
-class MyLoginView(LoginView):
+
+class LoginView(AuthLoginView):
     template_name = "accounts/login.html"
 
 
@@ -25,7 +23,7 @@ class RegistrationView(CreateView):
     template_name = "accounts/registration.html"
     success_url = reverse_lazy("accounts:index")
 
-class LogoutView(View):
-    def get(self, request):
-        logout(request)
 
+class LogoutView(View):
+    def get(self, request, *args, **kwargs):
+        logout(request)
